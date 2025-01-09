8a9a5068c6465d97cd82b8dcf65e03e8402251b5
AsH1605 <akshitatiwary16@gmail.com>
2025-01-09 15:07:47 +0530

    Final fine tuning model using LoRA

--- model.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-
-def load_model(model_name="JetBrains/deepseek-coder-1.3B-kexer"):
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name).to("cuda")
-    return tokenizer, model
